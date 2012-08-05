41c12b52324284efbaea59258f3fd13dbbc9f125
rinniss86 <rinniss86@gmail.com>
2012-08-05 09:34:33 -0400

    Re-organized files.

--- Android/src/com/example/testapp/MainActivity.java
+++ Android/src/com/example/testapp/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/launch.html");
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 
     @Override
